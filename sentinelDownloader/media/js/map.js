e3ba306079b7507f8dde4d7134155be33426e4b3
Matvey23 <artem.matviienko23@gmail.com>
2019-05-08 10:03:11 +0300

    create new polygon after click on button reset

--- sentinelDownloader/media/js/map.js
+++ sentinelDownloader/media/js/map.js
@@ -1,4 +1,4 @@
-let mymap = L.map('mapid').setView([51.505, -0.09], 13);
+let mymap = L.map('map').setView([51.505, -0.09], 13);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
     maxZoom: 18,
@@ -14,48 +14,61 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=p
 //     fillColor: '#f03',
 //     fillOpacity: 0.5
 // }).addTo(mymap).bindPopup("I am a circle.");
+//let ref = document.getElementById('map').innerHTML;
 
-
+const button = document.getElementById('button');
 const inputRange = document.forms[0].elements[0];
 const inputNumb = document.forms[0].elements[1];
 
-inputNumb.onchange = function(e){
+inputNumb.onchange = function (e) {
     inputRange.value = inputNumb.value;
 
 };
-inputRange.onchange = function(e){
+inputRange.onchange = function (e) {
     inputNumb.value = inputRange.value;
 
 };
 
 
-let dateDefaultStart=new Date();
+let dateDefaultStart = new Date();
 let dateDefaultFinish = new Date();
-dateDefaultStart.setDate(dateDefaultStart.getDate()-3);
+dateDefaultStart.setDate(dateDefaultStart.getDate() - 3);
 document.getElementById('finish').valueAsDate = dateDefaultFinish;
+document.getElementById('finish').maxAsDate = dateDefaultFinish;
 document.getElementById('start').valueAsDate = dateDefaultStart;
 let popup = L.popup();
 let position = [];
 
 
+
+
 function onMapClick(e) {
-    if (position.length<4){
-        position.push([e.latlng.lat,e.latlng.lng]);
+    if (position.length < 4) {
+        position.push([e.latlng.lat, e.latlng.lng]);
         L.marker([e.latlng.lat, e.latlng.lng]).addTo(mymap).openPopup();
+    } else {
+        return
     }
     popup
         .setLatLng(e.latlng)
-        .setContent( e.latlng.toString())
+        .setContent(e.latlng.toString())
         .openOn(mymap);
-    if (position.length===4 || position.length !== 3) {
+    if (position.length === 4 || position.length !== 3) {
         L.polygon(position).addTo(mymap).bindPopup("I am a polygon.");
     }
+    button.onclick = function (e) {
+    position = [];
+    console.log('Done');
+
+    //document.getElementById('map').innerHTML = ref;
+    //$(#map).load(location.href + "#map");
+
+};
+
 
     console.log(e.latlng.lat, e.latlng.lng);
 }
 
 mymap.on('click', onMapClick);
-
-
 
 
