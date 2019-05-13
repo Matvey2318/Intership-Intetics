3b1f95c404841d65b5b18789932ff2b0797baedb
Matvey23 <artem.matviienko23@gmail.com>
2019-05-13 15:30:16 +0300

    create ajax Request and create form for filling data-table

--- sentinelDownloader/media/js/app.js
+++ sentinelDownloader/media/js/app.js
@@ -2,10 +2,15 @@ var mapModel = new MapModel([51.505, -0.09]);
 $('#reset_map').click(function(){
     mapModel.resetMap();
 });
+
+
+
+
+
 let dict = {};
-let date_st = document.getElementById('start').value;
-let date_fin = document.getElementById('finish').value;
-let cloud = document.getElementById('cloud').value;
+let date_st = document.getElementById('start');
+let date_fin = document.getElementById('finish');
+let cloud = document.getElementById('cloud');
 cloud.onchange= function (e) {
     dataRecord();
 };
@@ -20,14 +25,26 @@ date_st.onchange= function (e) {
 
 function dataRecord (){
 
-    dict.start=date_st;
-    dict.finish=date_fin;
-    dict.cloud=cloud;
+    dict.start=date_st.value;
+    dict.finish=date_fin.value;
+    dict.cloud=cloud.value;
 
 };
 let sub = document.getElementById('sub');
 sub.onclick=function(e){
+    Request();
+};
 
-myWin = open('http://127.0.0.1:8000/data-table/')
+function Request(){
+    $.ajax({
+  type: "GET",
+  url: 'http://127.0.0.1:8000/app/home/findurls',
+  data: dict,
+  success: openDataTable ,
+  dataType:"number",
+});
+}
+function openDataTable(){
+    myWin = open('http://127.0.0.1:8000/data-table/')
 };
 
