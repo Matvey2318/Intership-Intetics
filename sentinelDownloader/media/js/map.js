f3c7def8b5a13d5e1de882911b2cb6df563e0127
Matvey23 <artem.matviienko23@gmail.com>
2019-05-15 13:28:03 +0300

    my change

--- sentinelDownloader/media/js/map.js
+++ sentinelDownloader/media/js/map.js
@@ -1,11 +1,11 @@
-
-
 var dateDefaultStart = new Date();
 var dateDefaultFinish = new Date();
 dateDefaultStart.setDate(dateDefaultStart.getDate() - 3);
 document.getElementById('finish').valueAsDate = dateDefaultFinish;
 document.getElementById('finish').maxAsDate = dateDefaultFinish;
 document.getElementById('start').valueAsDate = dateDefaultStart;
+
+var bounds = this.map
 
 var MapModel = class MapModel {
         constructor(center){
